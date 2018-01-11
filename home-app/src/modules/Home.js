4a81639b18e5c3fec3082e9fa82664c9909efe6b
DesaiSamir <desaisamirv@yahoo.com>
2018-01-11 09:10:16 -0500

    Add files via upload

---
+++ home-app/src/modules/Home.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+
+
+class Home extends Component {
+    render() {
+        return  <div>
+                This is my first react app. Home Page
+            </div>
+    }
+}
+
+export default Home;
